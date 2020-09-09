0a6a9b34e1c1f3c26024b20602ab570a6c8fd3b4
param999 <pgpro111@gmail.com>
2020-09-09 21:14:16 +0530

    wip

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,26 +1,57 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 import projectStyles from './projects.module.css';
 
-const Projects = () => (
-    <div className={projectStyles.projectContainer}>
-        <span className={projectStyles.projectTitle}>LATEST WORK</span>
-        <div>
-            <div>
-                <span>Beefit</span>
-                <div>
-                    Tech stack
+const Projects = () => {
+    const data = useStaticQuery(
+        graphql`
+            query {
+              allMarkdownRemark {
+                edges {
+                  node {
+                    id
+                    frontmatter {
+                      title
+                      techStack
+                      url
+                    }
+                    html
+                    rawMarkdownBody
+                  }
+                }
+              }
+            }
+        `
+    );
+
+    return(
+        <div className={projectStyles.projectContainer}>
+            <div className={projectStyles.projectTitle}>LATEST WORK</div>
+            <div className={projectStyles.projects}>
+                <div className={projectStyles.projectData}>
+                    <div className={projectStyles.projectName}>Beefit</div>
+                    <div className={projectStyles.stacks}>
+                        {
+                            data.allMarkdownRemark.edges[0].node.frontmatter.techStack.split(',').map(stack =>
+                                <div className={projectStyles.stack}>{stack}</div>
+                            )
+                        }
+                    </div>
+                    <div className={projectStyles.desc}>
+                        {
+                            data.allMarkdownRemark.edges[0].node.rawMarkdownBody
+                        }
+                    </div>
+                    <a className={projectStyles.link} href={data.allMarkdownRemark.edges[0].node.frontmatter.url} target="_blank" rel="noreferrer">
+                        see this project
+                    </a>
                 </div>
                 <div>
-                    description
+                    Images
                 </div>
-            </div>
-            <div>
-                Images
             </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default Projects;
-
-//https://youtu.be/TJ5CaIThKWk?t=813
--- src/markdown-pages/beefit.md
+++ src/markdown-pages/beefit.md
@@ -1,1 +1,9 @@
-test
+---
+title: "Beefit"
+techStack: "React JS, Redux, Apollo, Express JS, PostgreSQL, Hasura GQE, RazorPay, Firebase, Heroku, AWS"
+url: "http://ofdp-web.herokuapp.com"
+---
+
+
+
+Completely architectured and developed an online food delivery platform( BEEFIT) for the startup BEE LIFE SERVICES Private Limited MUMBAI. The Platform is planned to go live in the First week of October 2020. 
